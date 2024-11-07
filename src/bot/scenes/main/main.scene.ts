bef94827982a472e417adbf71576ff7ec945306b
mpnri <mpnri2831@gmail.com>
2024-11-08 01:38:20 +0330

    feat: Add status button

--- src/bot/scenes/main/main.scene.ts
+++ src/bot/scenes/main/main.scene.ts
@@ -2,6 +2,10 @@ import { Markup, Scenes } from "telegraf";
 import { goToMainScene, ScenesIDs as SceneIDs, ScenesIDs } from "../common";
 import type { BotContext } from "~/bot/session";
 import { prisma } from "~/db";
+import { digitsToEmoji, digitsToHindi } from "~/utils";
+
+const sceneReplyWithButtons = (ctx: BotContext, message: string) =>
+	ctx.reply(message, Markup.keyboard(["ثبت پیاده‌روی امروز", "مشاهده وضعیت"]));
 
 const mainScene = new Scenes.WizardScene<BotContext>(
 	SceneIDs.MainScene,
@@ -20,15 +24,36 @@ const mainScene = new Scenes.WizardScene<BotContext>(
 			ctx.session.id = user.id;
 		}
 
-		await ctx.reply(
-			"یک گزینه را انتخاب کنید.",
-			Markup.keyboard(["ثبت پیاده‌روی امروز", "مشاهده وضعیت"]),
-		);
+		await sceneReplyWithButtons(ctx, "یک گزینه را انتخاب کنید.");
 	},
 );
 
 mainScene.hears("ثبت پیاده‌روی امروز", async (ctx) => {
-	ctx.scene.enter(ScenesIDs.RecordTodayScene);
+	return ctx.scene.enter(ScenesIDs.RecordTodayScene);
+});
+mainScene.hears("مشاهده وضعیت", async (ctx) => {
+	const { id } = ctx.session;
+	if (!id) {
+		return sceneReplyWithButtons(ctx, "مشکلی پیش آمد. بات را مجددا استارت کنید.");
+	}
+	const user = await prisma.user.findUnique({
+		where: { id },
+		include: { walks: { orderBy: { date: "asc" } } },
+	});
+	if (!user) {
+		return sceneReplyWithButtons(ctx, "مشکلی پیش آمد. بات را مجددا استارت کنید.");
+	}
+	if (!user.walks.length) {
+		return sceneReplyWithButtons(ctx, "شما تا اکنون رکوردی ثبت نکردید.");
+	}
+	const status = user.walks
+		.map((walk, index) => {
+			return `${digitsToEmoji((index + 1).toString())} ${walk.date}\n🔸تعداد قدم‌ها: ${digitsToHindi(walk.count.toString())}`;
+		})
+		.join("\n\n");
+
+	const message = `📊وضعیت شما در ۳۰ روز گذشته:\n\n${status}`;
+	return sceneReplyWithButtons(ctx, message);
 });
 
 export { mainScene };
--- src/utils.ts
+++ src/utils.ts
@@ -45,6 +45,16 @@ export const digitsToLatin = (inputNumber: string): string => {
 	return result;
 };
 
+const digitEmojis = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"];
+
+export const digitsToEmoji = (inputString: string): string => {
+	let result = inputString;
+	new Array(10).fill(0).map((e, index) => {
+		result = result.replace(index.toString(), digitEmojis[index]);
+	});
+	return result;
+};
+
 export function toMoneyFormat(value: string): string {
 	const v = value.replace(/,/g, "");
 	if (!v) return "";
